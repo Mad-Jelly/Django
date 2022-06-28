667feee606ddd1d9a78e1107e2f4e7d0f32ccbf8
Mr.Jengz <apivat_c@hotmail.com>
2022-06-28 10:42:34 +0700

    การใช้ Loop

--- django/blogs/views.py
+++ django/blogs/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 # Create your views here.
 def hello(request):
-    
-    return render(request,'index.html',{'name':'บทความ Plannet','author':'Mr.Jengz'})
+    tags=['Django','pm2.5','Node-Red']
+    return render(request,'index.html',
+                  {
+                    'name':'บทความ Plannet',
+                    'author':'Mr.Jengz',
+                    'tags':tags
+                   })
