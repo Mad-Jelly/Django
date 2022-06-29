420018ca3e69ebae4f490b7bc38f6a0fe04c2f28
Mr.Jengz <apivat_c@hotmail.com>
2022-06-29 11:17:36 +0700

    สร้าง ADMIN เชื่อมต่อ Model ข้อมูลเข้ากับ admin.py  และลอง Qurey ด้วย Framwork

--- django/blogs/views.py
+++ django/blogs/views.py
@@ -1,7 +1,20 @@
 from unicodedata import name
 from django.shortcuts import render
+from .models import pm_collect
+import datetime
 # Create your views here.
+
 def hello(request):
+  #Query data from model
+  #data=pm_collect.objects.filter(pm_value=7.5)
+  data=pm_collect.objects.filter(pm_date__time__range=(datetime.time(11,4,00),datetime.time(11,5,13)))
+  return render(request,'index.html',
+  {
+    'pm':data
+    
+  })
+
+def page1(request):
   tags=['Django','pm2.5','Node-Red']
   rating=4
   return render(request,'index.html',
@@ -10,13 +23,7 @@ def hello(request):
     'name':'PM 2.5',
     'author':'Mr.Jengz',
     'tags':tags,
-    'rating':rating
-  })
-
-def page1(request):
-  return render(request,'page1.html',
-  {
-                                    
+    'rating':rating                              
   })
     
 def insert(request):
